99031cdfa885dfc93ce36cc0496b7fa966fcc585
luckyyii <144636344+luckyyii@users.noreply.github.com>
2023-09-10 17:37:40 -0700

    mini calculator

---
+++ mini calculator.py
@@ -0,0 +1,18 @@
+operator = input("enter ur operator (+ - * /): ")
+num1 = float(input("enter 1st #: "))
+num2 = float(input("enter 2nd #: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not correct")
