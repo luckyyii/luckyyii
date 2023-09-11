6ae8e7e802d0d9ccbdc9bc67cd64f76c8b51cb1e
luckyyii <144636344+luckyyii@users.noreply.github.com>
2023-09-10 18:47:35 -0700

    Update mini calculator.py
    updated code(help from friend)

--- mini calculator.py
+++ mini calculator.py
@@ -1,18 +1,27 @@
-operator = input("enter ur operator (+ - * /): ")
-num1 = float(input("enter 1st #: "))
-num2 = float(input("enter 2nd #: "))
+operator = input("Enter your operator (+ - * /): ")
+num1 = None
+num2 = None
 
-if operator == "+":
-    result = num1 + num2
-    print(round(result, 3))
-elif operator == "-":
-    result = num1 - num2
-    print(round(result, 3))
-elif operator == "*":
-    result = num1 * num2
-    print(round(result, 3))
-elif operator == "/":
-    result = num1 / num2
-    print(round(result, 3))
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+except ValueError:
+    print("Error: Invalid input. Please enter valid numbers.")
 else:
-    print(f"{operator} is not correct")
+    if operator == "+":
+        result = num1 + num2
+        print(round(result, 3))
+    elif operator == "-":
+        result = num1 - num2
+        print(round(result, 3))
+    elif operator == "*":
+        result = num1 * num2
+        print(round(result, 3))
+    elif operator == "/":
+        if num2 == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            result = num1 / num2
+            print(round(result, 3))
+    else:
+        print(f"{operator} is not a valid operator.")
